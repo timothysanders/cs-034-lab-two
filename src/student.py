2450e6c03a1158486f02c519bfb6c7b3635674aa
Tim Sanders <sanders.timothy.j@gmail.com>
2025-03-01 20:32:09 -0800

    Added get_average() method, updated __str__ method logic

--- src/student.py
+++ src/student.py
@@ -32,31 +32,57 @@ class Student:
     def get_id(self):
         return self._id
 
-    #def add_grade(self, course, grade):
-    #    """Adds a valid grade for a given course"""
-    #    pass
-        # try:
-        #     if not isinstance(grade, (int, float)):
-        #         raise ValueError("Grade must be a numerical value.")
-        #     if not (0 <= grade <= 100):
-        #         raise ValueError("Grade must be between 0 and 100.")
-
-        #     self.__grades[course] = grade  # Store valid grade
-        #     print(f"Grade {grade} added for {course}")
-
-        # except ValueError as e:
-        #     print(f"Error: {e}")
-
-    # def get_grades(self):
-    #     return self._grades
-
     def get_average(self):
-        pass
-        # if not self._grades:
-        #     return 0  # Return 0 if no grades are available
-        # return sum(self._grades.values()) / len(self._grades)
+        total_score = 0
+        total_credits = 0
+        overall_letter_grade = "F"
+        overall_gpa = 0.0
+        for _, grade in self.get_grades().items():
+            total_score += grade["grade"] * grade["credits"]
+            total_credits += grade["credits"]
+        average_score = total_score / total_credits
+        if average_score >= 97:
+            overall_letter_grade = "A+"
+            overall_gpa = 4.0
+        elif average_score >= 93:
+            overall_letter_grade = "A"
+            overall_gpa = 4.0
+        elif average_score >= 90:
+            overall_letter_grade = "A-"
+            overall_gpa = 3.7
+        elif average_score >= 87:
+            overall_letter_grade = "B+"
+            overall_gpa = 3.3
+        elif average_score >= 83:
+            overall_letter_grade = "B"
+            overall_gpa = 3.0
+        elif average_score >= 80:
+            overall_letter_grade = "B-"
+            overall_gpa = 2.7
+        elif average_score >= 77:
+            overall_letter_grade = "C+"
+            overall_gpa = 2.3
+        elif average_score >= 73:
+            overall_letter_grade = "C"
+            overall_gpa = 2.0
+        elif average_score >= 70:
+            overall_letter_grade = "C-"
+            overall_gpa = 1.7
+        elif average_score >= 67:
+            overall_letter_grade = "D+"
+            overall_gpa = 1.3
+        elif average_score >= 65:
+            overall_letter_grade = "D"
+            overall_gpa = 1.0
+        return {"calculated_letter_grade": overall_letter_grade, "calculated_gpa": overall_gpa}
+
 
     def __str__(self):
         """Return a formatted string representation of the given student"""
-        return f"Student: {self._name} (ID: {self._id} | Grades: N/A)"
+        if self.get_grades() == {}:
+            string_message = f"Student: {self._name} (ID: {self._id} | Overall Grade: N/A | Overall GPA: N/A)"
+        else:
+            results = self.get_average()
+            string_message = f"Student: {self._name} (ID: {self._id} | Overall Grade: {results["calculated_letter_grade"]} | Overall GPA: {results["calculated_gpa"]})"
+        return string_message
 
